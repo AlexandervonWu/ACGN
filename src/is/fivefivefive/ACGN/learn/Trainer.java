2c56c1b2da163a16ab56771b15d17d2d0ed34658
Guanxuan Wu <devturchine@gmail.com>
2024-07-05 02:37:49 -0500

    add removed

---
+++ src/is/fivefivefive/ACGN/learn/Trainer.java
@@ -0,0 +1,15 @@
+package is.fivefivefive.ACGN.learn;
+
+import java.util.List;
+
+import is.fivefivefive.ACGN.asg.Multigraph;
+
+public class Trainer {
+    public static List<Double> train(List<Multigraph> trainSet, List<Multigraph> validation, List<Double> init, double lr) {
+        // TODO
+        for (Multigraph model : trainSet) {
+            
+        }
+        return null;
+    }
+}
