819e99d3203d07496a238c231ba41157d7709e63
Guanxuan Wu <devturchine@gmail.com>
2024-08-21 09:33:06 -0500

    fromAST

--- src/is/fivefivefive/ACGN/asg/AugmentedNode.java
+++ src/is/fivefivefive/ACGN/asg/AugmentedNode.java
@@ -9,13 +9,22 @@ public class AugmentedNode {
     private int syntactic;
     private double semantic;
     private double signature;
+    private boolean isShadow;
     public AugmentedNode(int syntactic, int semantic) {
         this.syntactic = syntactic;
         this.semantic = semantic;
+        this.isShadow = false;
     }
     public AugmentedNode(NodeRepresentation nr) {
         this.syntactic = nr.getSyntacticRepresentation();
         this.semantic = nr.getSemanticRepresentation();
+        this.isShadow = false;
+    }
+    // Create a shadow node to resolve self loops
+    public AugmentedNode(AugmentedNode original) {
+        this.syntactic = original.getSyntactic();
+        this.semantic = original.getSemantic();
+        this.isShadow = true;
     }
     public int getSyntactic() {
         return syntactic;
@@ -31,6 +40,9 @@ public class AugmentedNode {
     }
     public void initSignature() {
         this.signature = 0.0;
+    }
+    public boolean isShadow() {
+        return isShadow;
     }
     @Override
     public boolean equals(Object o) {
--- src/is/fivefivefive/ACGN/asg/Multigraph.java
+++ src/is/fivefivefive/ACGN/asg/Multigraph.java
@@ -1,9 +1,21 @@
 package is.fivefivefive.ACGN.asg;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 import is.fivefivefive.alloyasg.asg.ASGVisitor;
+import is.fivefivefive.alloyasg.asg.ASGraph;
+import is.fivefivefive.alloyasg.etc.DoubleMap;
+import is.fivefivefive.alloyasg.exceptions.ScopeNotFoundException;
+import is.fivefivefive.alloyasg.exceptions.UnsupportedConstantException;
+import is.fivefivefive.alloyasg.representations.NodeRepresentation;
+import parser.ast.nodes.Node;
 import is.fivefivefive.ACGN.asg.Multigraph;
 
 /*
@@ -36,8 +48,44 @@ public class Multigraph {
     public int size() {
         return vertices.size();
     }
-    public static Multigraph fromAST(ASGVisitor<Object> visitor, int root) {
-        // TODO: Implement this method
-        return null;
+    public static Multigraph fromAST(ASGVisitor<Object> visitor, int root) throws ScopeNotFoundException, UnsupportedConstantException {
+        ASGraph ast = visitor.getGraph();
+        DoubleMap<Integer, Node> nodeMap = visitor.getNodeMap();
+        Node rootNode = nodeMap.get(root);
+        NodeRepresentation rootRep = new NodeRepresentation(visitor, rootNode);
+        AugmentedNode rootAug = new AugmentedNode(rootRep);
+        Set<AugmentedNode> vertices = new HashSet<AugmentedNode>();
+        List<MASGEdge> edges = new ArrayList<MASGEdge>();
+        vertices.add(rootAug);
+        Queue<Integer> nodeQueue = new LinkedList<Integer>();
+        nodeQueue.add(root);
+        Map<NodeRepresentation, Integer> timeOfVisit = new HashMap<NodeRepresentation, Integer>();
+        while (!nodeQueue.isEmpty()) {
+            int localRoot = nodeQueue.poll();
+            double[] rootRow = ast.getRow(localRoot);
+            for (int i = 0; i < rootRow.length; i++) {
+                Node n = nodeMap.get(i);
+                NodeRepresentation nr = new NodeRepresentation(visitor, n);
+                if (rootRow[i] > 0) {
+                    if (timeOfVisit.containsKey(nr)) {
+                        timeOfVisit.put(nr, timeOfVisit.get(nr) + 1);
+                    } else {
+                        timeOfVisit.put(nr, 1);
+                    }
+                    if (i == localRoot) {
+                        AugmentedNode shadow = new AugmentedNode(rootAug);
+                        vertices.add(shadow);
+                        edges.add(new MASGEdge(rootAug, shadow, i, timeOfVisit.get(nr)));
+                        continue;
+                    }
+
+                    AugmentedNode an = new AugmentedNode(nr);
+                    vertices.add(an);
+                    nodeQueue.add(i);
+                    edges.add(new MASGEdge(rootAug, an, i, timeOfVisit.get(nr)));
+                }
+            }
+        }
+        return new Multigraph(vertices, edges);
     }
 }
