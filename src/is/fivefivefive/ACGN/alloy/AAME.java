742d4e081708ea6e3f9ae878335513cae7471718
Guanxuan Wu <devturchine@gmail.com>
2025-03-14 18:56:50 -0500

    aaaa

--- src/is/fivefivefive/ACGN/alloy/AAME.java
+++ src/is/fivefivefive/ACGN/alloy/AAME.java
@@ -10,16 +10,32 @@ import is.fivefivefive.alloyasg.asg.ASGVisitor;
 public class AAME {
     // TODO
     private List<Symbol> symbols;
-
+    private List<ExtFact> facts;
     public AAME(ASGVisitor<Object> asgv) {
         //TODO: Find all of the nonpredicate symbols in the Alloy environment and put into the AAME
         
     }
+    public AAME() {}
     public void fetchFromCloud(String cloud) {
         // TODO: Setup a database
     }
     // TODO: IMPLEMENT FACTS AND FIELD IMPLICIT FACTS. 
     public List<Symbol> getSymbols() {
         return symbols;
+    }
+    public List<ExtFact> getFacts() {
+        return facts;
+    }
+    public void setSymbols(List<Symbol> symbols) {
+        this.symbols = symbols;
+    }
+    public void setFacts(List<ExtFact> facts) {
+        this.facts = facts;
+    }
+    public void addSymbol(Symbol s) {
+        symbols.add(s);
+    }
+    public void addFact(ExtFact f) {
+        facts.add(f);
     }
 }
--- src/is/fivefivefive/ACGN/visitor/MASGVisitor.java
+++ src/is/fivefivefive/ACGN/visitor/MASGVisitor.java
@@ -2,16 +2,21 @@ package is.fivefivefive.ACGN.visitor;
 import java.util.List;
 import java.util.ArrayList;
 import is.fivefivefive.ACGN.asg.Multigraph;
+import is.fivefivefive.ACGN.alloy.AAME;
 import parser.ast.visitor.VoidVisitorAdapter;
 public class MASGVisitor<A> extends VoidVisitorAdapter<A> {
     // forest: the ASG forest of the predicates within the model
     private List<Multigraph> forest;
+    private AAME aame;
 
     public MASGVisitor() {
         forest = new ArrayList<Multigraph>();
     }
     public List<Multigraph> getForest() {
         return forest;
+    }
+    public AAME getAAME() {
+        return aame;
     }
     // visits, all non-predicates are discarded. 
     // consider AAME into it. 
